f3f4c65b05187640ea98d1fb0d3fa9ad84fc470c
shangzhaozhao <3064159737@qq.com>
2023-07-25 15:08:39 +0800

    chore: 配置antdesign vue-request
    配置antdesign vue-request

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,10 @@ import { createApp } from "vue";
 import App from "@/App.vue";
 import router from "@/router";
 import "./styles.css";
-import Antd from "ant-design-vue";
 import "ant-design-vue/dist/reset.css";
 import { Icon } from "@iconify/vue";
 
 const app = createApp(App);
 app.use(router);
-app.use(Antd);
 app.component("Icon", Icon);
 app.mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,31 @@
 const { defineConfig } = require("@vue/cli-service");
 const AutoImport = require("unplugin-auto-import/webpack");
+const { AntDesignVueResolver } = require("unplugin-vue-components/resolvers");
+const AutoImportComponents = require("unplugin-vue-components/webpack");
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
     plugins: [
       AutoImport({
         dts: true,
-        imports: ["vue", "vue-router"],
+        imports: [
+          "vue",
+          "vue-router",
+          {
+            "vue-request": ["useRequest "],
+          },
+        ],
         eslintrc: {
           enabled: true,
         },
+      }),
+      AutoImportComponents({
+        dts: true,
+        resolvers: [
+          AntDesignVueResolver({
+            importStyle: false, // css in js
+          }),
+        ],
       }),
     ],
   },
